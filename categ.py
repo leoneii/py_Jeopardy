1a0e5429f5c2215d0bb70c5248359bcd90500898
Ed <ednet@mail.ru>
2023-05-29 01:13:01 +0300

    с домашнего компа

---
+++ categ.py
@@ -0,0 +1,75 @@
+import sys
+import PySide6
+from PySide6 import QtCore
+# import PySide6
+from PySide6.QtCore import (QTimer, QEventLoop, QRect, Qt, QEvent, QPoint)
+from PySide6.QtGui import (QColor, QMouseEvent, QFont, QPalette, QPainter, QPen, QLinearGradient)
+from PySide6.QtSql import QSqlDatabase, QSqlQuery
+from PySide6.QtWidgets import (QApplication, QLabel, QWidget, QFrame, QPushButton)
+from PySide6.QtWidgets import QMessageBox
+# забирать из базы данных
+ckols=5
+catname=["Московские бульвары","Проспекты","Набережные","Московский метрополитен","Музеи Москвы","Кино","Транспорт","Парки"]
+blc=80
+shag=1
+smx=5
+gx=200
+
+#конец блока переменных
+class Category(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+    # def __init__(self, ckols, parent=None):
+    #     super(Category, self).__init__(parent)
+        geometry = apc.primaryScreen().availableGeometry()
+        self.setGeometry(geometry)
+
+#Анимация фона        
+        def scrupd():
+            global blc, shag
+            blc+=shag
+            if blc>=254 or blc<=50:
+                shag*= -1
+            self.sth="background-color: rgba(0,0,"+str(blc)+",255); color: #ddFFaa;"
+            self.setStyleSheet(self.sth)
+
+        self.tmr = QTimer()  # 4
+        self.tmr.timeout.connect(scrupd)
+        self.tmr.start(40)        
+#конец анимации фона        
+        
+
+            
+
+        wd = self.size().width()-20
+        hd = self.size().height()-20
+        wdb=wd/3
+        hdb=(hd-50)/6
+        if ckols>6:
+            hdb=(hd-50)/ckols
+        hpos=((hd-50)-(hdb+5)*ckols)/2
+
+        shst="QPushButton { background-color: rgba(0,0,200,100); color: rgba(220,220,255,255); text-align:center center; background-position: bottom center; border: 2px solid rgb(160, 180, 250); border-radius: 12px; font: Bold 38px} QPushButton::hover{background-color: #0077ff ;}"
+        def catchois():
+            # snd = self.sender()
+            # print(snd)
+            self.destroy()
+
+        for i in range(ckols):
+
+            cbut=QPushButton(catname[i],self)
+            cbut.setGeometry(wdb,hpos+(hdb+5)*i+35,wdb,hdb)
+            cbut.setObjectName("cat"+str(i))
+            cbut.setStyleSheet(shst)
+            cbut.clicked.connect(catchois)
+            cbut.show()
+
+        
+ 
+
+apc=QApplication([])
+wnc=Category()
+wnc.showFullScreen()
+sys.exit(apc.exec())
--- street_team.py
+++ street_team.py
@@ -4,13 +4,15 @@ from PySide6.QtGui import QFont, QPixmap
 from PySide6.QtWidgets import QWidget, QApplication, QLabel, QPushButton, QMessageBox
 
 # забирать из базы данных
-tkolt=4
-cenv=30
-tots=[0,0,0,0,0,0]
-name=["Средняя общеобразовательная школа №1316","Средняя общеобразовательная школа №753","Центр образования №951","СОШ №531","Средняя общеобразовательная школа №764","Средняя общеобразовательная школа №786"]
-logo=["1316.jpg","","753.jpg","951.jpg","531.jpg","",""]
-#
+tkolt = 4
+cenv = 30
+tots = [0, 0, 0, 0, 0, 0]
+name = ["Средняя общеобразовательная школа №1316", "Средняя общеобразовательная школа №753", "Центр образования №951",
+        "СОШ №531", "Средняя общеобразовательная школа №764", "Средняя общеобразовательная школа №786"]
+logo = ["1316.jpg", "", "753.jpg", "951.jpg", "531.jpg", "", ""]
 
+
+#
 
 
 class Wint(QWidget):
@@ -20,12 +22,12 @@ class Wint(QWidget):
         font.setFamilies([u"Arial"])
         font.setBold(True)
         alignmentc = Qt.AlignmentFlag.AlignCenter
-        tkol=tkolt
-        snd=""
+        tkol = tkolt
+        snd = ""
         geometry = apt.primaryScreen().availableGeometry()
         self.setGeometry(geometry)
         wdt = self.size().width()
-        twdt=(wdt-20)/tkol-10
+        twdt = (wdt - 20) / tkol - 10
         hgt = self.size().height()
         self.setAutoFillBackground(True)
         self.setStyleSheet("""
@@ -37,87 +39,84 @@ class Wint(QWidget):
         def cntn(self):
             apt.quit()
 
-
         for i in range(tkol):
-            if len(logo[i])>0:
-                self.logo=QLabel(self)
-                self.logo.setGeometry(i*(twdt+10)+10,10,twdt,hgt/4-10)
+            if len(logo[i]) > 0:
+                self.logo = QLabel(self)
+                self.logo.setGeometry(i * (twdt + 10) + 10, 10, twdt, hgt / 4 - 10)
                 self.logo.setAlignment(alignmentc)
                 pixmap = QPixmap("img/logo/" + logo[i])
-                pixmap = pixmap.scaled(twdt, hgt/4 -20, Qt.KeepAspectRatio)
+                pixmap = pixmap.scaled(twdt, hgt / 4 - 20, Qt.KeepAspectRatio)
                 self.logo.setPixmap(pixmap)
                 self.logo.setStyleSheet("border:3px solid #bbaaff;font-size: 48px")
                 self.logo.show()
 
-            self.tnm=QLabel(self)
+            self.tnm = QLabel(self)
             if len(logo[i]) > 0:
-                self.tnm.setGeometry(i*(twdt+10)+10,10+hgt/4,twdt,hgt/3-20)
+                self.tnm.setGeometry(i * (twdt + 10) + 10, 10 + hgt / 4, twdt, hgt / 3 - 20)
             else:
-                self.tnm.setGeometry(i * (twdt + 10) + 10, 10 , twdt, hgt / 3+hgt/4 - 20)
+                self.tnm.setGeometry(i * (twdt + 10) + 10, 10, twdt, hgt / 3 + hgt / 4 - 20)
             self.tnm.setWordWrap(True)
             self.tnm.setText(name[i])
             self.tnm.setAlignment(alignmentc)
             self.tnm.setFont(font)
-            fnts=60-tkolt*6
-            stsh="border:3px solid #bbaaff;font-size: "+str(fnts)+"px"
+            fnts = 60 - tkolt * 6
+            stsh = "border:3px solid #bbaaff;font-size: " + str(fnts) + "px"
             self.tnm.setStyleSheet(stsh)
             self.tnm.show()
 
-            self.result=QLabel(self)
+            self.result = QLabel(self)
             self.result.setObjectName("rst" + str(i))
-            self.result.setGeometry(i*(twdt+10)+10,hgt/4+hgt/3,twdt,hgt/5)
+            self.result.setGeometry(i * (twdt + 10) + 10, hgt / 4 + hgt / 3, twdt, hgt / 5)
             self.result.setWordWrap(True)
             self.result.setText(str(tots[i]))
             self.result.setAlignment(alignmentc)
             self.result.setFont(font)
-            fnts1=96-tkolt*5
-            stsh="border:3px solid #bbaaff;font-size: "+str(fnts1)+"px"
+            fnts1 = 96 - tkolt * 5
+            stsh = "border:3px solid #bbaaff;font-size: " + str(fnts1) + "px"
             self.result.setStyleSheet(stsh)
             self.result.show()
 
-
-
-            self.plusb=QPushButton(self)
-            self.plusb.setObjectName("pls"+str(i))
-            #QMessageBox.warning(self, "Нажматие!!!))", "Название объекта " + str(self.plusb.objectName()))
-            self.plusb.setGeometry(i*(twdt+10)+10,hgt/4+hgt/3+hgt/5+10,twdt/2-2,hgt/10)
+            self.plusb = QPushButton(self)
+            self.plusb.setObjectName("pls" + str(i))
+            # QMessageBox.warning(self, "Нажматие!!!))", "Название объекта " + str(self.plusb.objectName()))
+            self.plusb.setGeometry(i * (twdt + 10) + 10, hgt / 4 + hgt / 3 + hgt / 5 + 10, twdt / 2 - 2, hgt / 10)
             self.plusb.setText("+")
-            self.plusb.setStyleSheet("font-size: "+str(fnts1)+"px")
+            self.plusb.setStyleSheet("font-size: " + str(fnts1) + "px")
             self.plusb.clicked.connect(self.sumf)
             self.plusb.show()
 
-
-            self.minub=QPushButton(self)
+            self.minub = QPushButton(self)
             self.minub.setObjectName("mns" + str(i))
-            self.minub.setGeometry(i*(twdt+10)+12+twdt/2,hgt/4+hgt/3+hgt/5+10,twdt/2-2,hgt/10)
+            self.minub.setGeometry(i * (twdt + 10) + 12 + twdt / 2, hgt / 4 + hgt / 3 + hgt / 5 + 10, twdt / 2 - 2,
+                                   hgt / 10)
             self.minub.setText("-")
-            self.minub.setStyleSheet("font-size: "+str(fnts1)+"px")
+            self.minub.setStyleSheet("font-size: " + str(fnts1) + "px")
             self.minub.clicked.connect(self.sumf)
             self.minub.show()
 
         self.contin = QPushButton(self)
-        self.contin.setGeometry(10, hgt-hgt/15-10, wdt  - 20,
-                               hgt / 15)
+        self.contin.setGeometry(10, hgt - hgt / 15 - 10, wdt - 20,
+                                hgt / 15)
         self.contin.setText("К вопросам")
         self.contin.setStyleSheet("font-size: " + str(fnts) + "px")
         self.contin.clicked.connect(cntn)
         self.contin.show()
 
-
     def sumf(self):
         sndr = self.sender().objectName()
-       # QMessageBox.warning(self, "Нажматие!!!))", " " + str(self.sender().objectName()))
+        # QMessageBox.warning(self, "Нажматие!!!))", " " + str(self.sender().objectName()))
         if sndr[:3] == "pls":
-            tots[int(sndr[3:])]+=cenv
+            tots[int(sndr[3:])] += cenv
         else:
             tots[int(sndr[3:])] -= cenv
-#меняем значения на лейблах
+        # меняем значения на лейблах
         obj = self.findChild(QLabel, "rst" + sndr[3:])
         obj.setText(str(tots[int(sndr[3:])]))
 
-apt=QApplication([])
-wnt=Wint(tkolt)
+
+apt = QApplication([])
+wnt = Wint(tkolt)
 #    wnt.setStyleSheet('background: black')
 wnt.showFullScreen()
-    # apt.exec()
-sys.exit(apt.exec())
+apt.exec()
+#sys.exit(apt.exec())
