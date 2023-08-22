92be36252e83742cd0cf05b01a508b5cfb8d1286
leoneii <comdir@infonix.info>
2023-08-22 12:57:28 +0300

    Обновил

--- backend/backend.py
+++ backend/backend.py
@@ -1,7 +1,9 @@
 # This Python file uses the following encoding: utf-8
+import logging
 import sys
+from PySide6.QtSql import QSqlDatabase, QSqlQuery
 
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QInputDialog, QMainWindow
 
 # Important:
 # You need to run the following command to generate the ui_form.py file
@@ -13,11 +15,58 @@ class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.ui = Ui_MainWindow()
+        sqlDB = QSqlDatabase.addDatabase('QSQLITE')
+        sqlDB.setDatabaseName('/home/leone/build/testpy8/backend/jep.sqlite')
+        sqlDB.open()
         self.ui.setupUi(self)
+        self.ui.pushButton_addCat.clicked.connect(self.addCat)
+        self.ui.comboBox_Cat.currentIndexChanged.connect(self.catChange)
+        self.updateform()
+        
+    def updateform(self):
+        self.ui.comboBox_Cat.clear()
+        query = QSqlQuery()
+        if not query.exec("SELECT * FROM category;"):
+            logging.error("Failed to query database")  
+        while(query.next()):
+            self.ui.comboBox_Cat.addItem(query.value(1))
+        
+    def catChange(self):
+        #       Высчитываем количество тем и вопросов
+        query2 = QSqlQuery()
+        if not query2.exec("SELECT COUNT(DISTINCT Theme) FROM ThemeAndQ WHERE Catkod = " + str(self.ui.comboBox_Cat.currentIndex()+1) + " ;"):
+            logging.error("Failed to query database")
+        query2.first()
+        self.ui.spinBox_themes.setValue(int(query2.value(0)))
 
+        query2 = QSqlQuery()
+        if not query2.exec("SELECT Theme, COUNT(*) value_count FROM ThemeAndQ WHERE Catkod = " + str(self.ui.comboBox_Cat.currentIndex()+1) + " GROUP BY Theme HAVING value_count > 1  ;"):
+            logging.error("Failed to query database")
+        query2.first()
+        self.ui.spinBox_questions.setValue(int(query2.value(1)))
+
+        query = QSqlQuery()
+        if not query.exec("SELECT * FROM ThemeAndQ;"):
+            logging.error("Failed to query database")  
+        while(query.next()):
+            print("vchjd")
+        
+    
+    def addCat(self):
+        texcat = QInputDialog.getText(None,"PyG","Введите наименование категории"); 
+        query = QSqlQuery()
+        query2 = QSqlQuery()
+        if not query.exec("SELECT MAX(catcode) FROM category;"):
+                logging.error("Failed to query database") 
+        query.first()  
+        print(query.value(0))         
+        if texcat[1] == True:
+            if not query2.exec("INSERT INTO category  VALUES ("+str(int(query.value(0))+1)+", '"+texcat[0]+"');"):
+                 logging.error("Failed to query database")
+        self.updateform()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     widget = MainWindow()
     widget.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
