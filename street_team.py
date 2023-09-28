b2023b28090f66cd5d4a4dd4122b5d72afeb250e
Ed <ednet@mail.ru>
2023-09-29 00:00:13 +0300

    с домашнего компа

--- street_team.py
+++ street_team.py
@@ -1,7 +1,7 @@
 import logging
 import os
 import sys
-#import PySide6
+# import PySide6
 from PySide6 import QtCore
 from PySide6.QtCore import Qt, QPoint, QTimer, QEvent
 from PySide6.QtGui import (QColor, QMouseEvent, QFont, QPalette, QPainter, QPen, QLinearGradient, QPixmap)
@@ -13,25 +13,25 @@ import widget
 from categ import Category
 from widget import wnd, cenv
 
-#global apt
-#база данных
-#global sqlDB
+global apt
+# база данных
+# global sqlDB
 QtCore.QLocale.setDefault(QtCore.QLocale("ru_RU"))
 # sqlDB = QSqlDatabase.addDatabase('QSQLITE')
 # sqlDB.setDatabaseName('jep.sqlite')
 # sqlDB.open()
-name=["","","","","","","",""]
-logo=["","","","","","","",""]
-lcnnxt=30
+name = ["", "", "", "", "", "", "", ""]
+logo = ["", "", "", "", "", "", "", ""]
+lcnnxt = 30
 query = QSqlQuery()
 if not query.exec(
         """
             SELECT COUNT(*) FROM Teams;
         """
 ):
-        logging.error("Failed to query database16")
+    logging.error("Failed to query database16")
 query.next()
-tkolt=int(query.value(0)) # количество команд
+tkolt = int(query.value(0))  # количество команд
 
 query = QSqlQuery()
 if not query.exec(
@@ -39,37 +39,36 @@ if not query.exec(
             SELECT * FROM Teams;
         """
 ):
-        logging.error("Failed to query database17")
+    logging.error("Failed to query database17")
 query.first()
 
-s=""
+s = ""
 for i in range(tkolt):
-    logo[i]=str(query.value(1))
-    name[i]=str(query.value(2))
-    s+=name[i]+" "
+    logo[i] = str(query.value(1))
+    name[i] = str(query.value(2))
+    s += name[i] + " "
     query.next()
-#вычисляем кол-во букв в самом длинном слове в названиях команд
-maxlenw= max(s.split(), key=len)
-lmaxlw=len(maxlenw)
-
+# вычисляем кол-во букв в самом длинном слове в названиях команд
+maxlenw = max(s.split(), key=len)
+lmaxlw = len(maxlenw)
 
 mascat = []
 tots = [0, 0, 0, 0, 0, 0]
 
-blc=120
-shag=2
-smx=5
-gx=200
-cnttxt=""
+blc = 120
+shag = 2
+smx = 5
+gx = 200
+cnttxt = ""
 global geometry
-ccat=1
+ccat = 1
 
-cenv=0
+cenv = 0
+
 
 class Wint(QWidget):
     global cnttxt, cntcode, ccat
     # забираем название категории из settings
-
 
     # query = QSqlQuery()
     # if not query.exec(
@@ -83,11 +82,13 @@ class Wint(QWidget):
 
     # рисуем анимацию фона
     global cenv
+
     def paintEvent(self, event):
         global smx
         global gx
 
         painter = QPainter(self)
+        #  painter.begin(self)
         x = 0
         y = 0
         wd = self.size().width()
@@ -107,7 +108,7 @@ class Wint(QWidget):
         pen.setColor(QColor(0, 0, 50, 10))
         painter.setPen(pen)
         painter.drawRect(x, y, wd, hd)
-    # закончили с фоном
+        # закончили с фоном
         query = QSqlQuery()
         if not query.exec(
                 """
@@ -122,78 +123,77 @@ class Wint(QWidget):
     def __init__(self, tkol, parent=None):
         global lmaxlw, ccat
         sqlDB = QSqlDatabase.addDatabase('QSQLITE')
-        sqlDB.setDatabaseName(os.path.dirname(os.path.abspath(__file__))+"/./jep.sqlite")
+        sqlDB.setDatabaseName(os.path.dirname(os.path.abspath(__file__)) + "/./jep.sqlite")
         sqlDB.open()
-        # запись в settings параметров экрана
-        geometry = apt.primaryScreen().availableGeometry()
-        quec = QSqlQuery()
-        if not quec.exec("UPDATE settings set avaHeight = '"+str(geometry.height())+"', avaWidth = '"+str(geometry.width())+"';"):
-            logging.error("Failed to query database27")
-        #quec.next()  
         super(Wint, self).__init__(parent)
-        #super().__init__()
+        # super().__init__()
         font = QFont()
         font.setFamilies([u"Arial"])
         font.setBold(True)
         alignmentc = Qt.AlignmentFlag.AlignCenter
         tkol = tkolt
         snd = ""
+        geometry = apt.primaryScreen().availableGeometry()
         self.setGeometry(geometry)
         wdt = self.size().width()
         twdt = (wdt - 20) / tkol - 10
-        fw=int(twdt/lmaxlw)
+        fw = int(twdt / lmaxlw)
         hgt = self.size().height()
         self.setAutoFillBackground(True)
-        mainlogoh=hgt/5
-        self.mainlogo=QLabel(self)
-        self.mainlogo.setGeometry(10,5,wdt-30,mainlogoh)
-        pixmap = QPixmap(os.path.dirname(os.path.abspath(__file__))+"/./img/logo/logo.png")
-        pixmap = pixmap.scaled(1200, hgt / 5-15 , Qt.KeepAspectRatio)
+        mainlogoh = hgt / 5
+        self.mainlogo = QLabel(self)
+        self.mainlogo.setGeometry(10, 5, wdt - 30, mainlogoh)
+        pixmap = QPixmap(os.path.dirname(os.path.abspath(__file__)) + "/./img/logo/logo.png")
+        pixmap = pixmap.scaled(wdt / 3, hgt / 5 - 15, Qt.KeepAspectRatio)
         self.mainlogo.setPixmap(pixmap)
         self.mainlogo.setAlignment(alignmentc)
         self.sth = "background-color: rgba(0,0," + str(blc) + ",0);"
         self.mainlogo.setStyleSheet(self.sth)
         self.mainlogo.show()
-#Насыщенность фона        
+        hgt = hgt - mainlogoh
+
+        # self.setStyleSheet("""
+        #         background-color: #0000cc;
+        #         color: #ddFFaa;
+        #         font-family: Arial;
+        #         """)
+        # Насыщенность фона
         def scrupd():
             global blc, shag
-            blc+=shag
-            if blc>=254 or blc<=90:
-                shag*= -1
-            self.sth="background-color: rgba(0,0,"+str(blc)+",75); color: #ddFFaa;"
+            blc += shag
+            if blc >= 254 or blc <= 90:
+                shag *= -1
+            self.sth = "background-color: rgba(0,0," + str(blc) + ",75); color: #ddFFaa;"
             self.setStyleSheet(self.sth)
 
         self.tmr = QTimer()  # 4
         self.tmr.timeout.connect(scrupd)
-        self.tmr.start(40)        
-#конец насыщенности фона
-        
+        self.tmr.start(40)
+        # конец насыщенности фона
+
         queryccat = QSqlQuery()
         if not queryccat.exec("SELECT COUNT(*) FROM category;"):
-            logging.error("Failed to query database15")  
+            logging.error("Failed to query database15")
         queryccat.first()
-        ccat= queryccat.value(0)    
+        ccat = queryccat.value(0)
 
-
-
-#создаем экраны вопросов категорий
+        # создаем экраны вопросов категорий
         query = QSqlQuery()
         if not query.exec("SELECT catname FROM category;"):
-            logging.error("Failed to query database1")  
-        global mascat    
-        while(query.next()):
-            cwnd = wnd(query.value(0),apt)
-            #print(query.value(0))
+            logging.error("Failed to query database1")
+        global mascat
+        while (query.next()):
+            cwnd = wnd(query.value(0), apt)
+            # print(query.value(0))
             cwnd.setVisible(False)
-            #cwnd.setObjectName("widget_"+str(i))
+            # cwnd.setObjectName("widget_"+str(i))
             mascat.append(cwnd)
-        
+
             # создаем виджет - один навсегда))
 
+        # конец init
 
- # конец init
-
-        #ПРОДОЛЖИТЬ
+        # ПРОДОЛЖИТЬ
 
         def cntn():
             global cntname
@@ -203,52 +203,53 @@ class Wint(QWidget):
                 logging.error("Failed to query database22")
             query.first()
             cntname = str(query.value(8))
-            
+
             querycatn = QSqlQuery()
             if not querycatn.exec("SELECT ROW_NUMBER() OVER() as row_number,catname FROM Category ;"):
                 logging.error("Failed to query database21")
-            #querycatn.first()
+            # querycatn.first()
             while (querycatn.next()):
                 if (querycatn.value(1) == cntname):
-                    mascat[int(querycatn.value(0))-1].showFullScreen()
-
-            
+                    mascat[int(querycatn.value(0)) - 1].showFullScreen()
 
         def chcat(self):
             global cnttxt, cntname, lcnnxt
 
-            vcat=Category(apt)
+            vcat = Category(apt)
             vcat.showFullScreen()
             query = QSqlQuery()
             if not query.exec("SELECT * FROM settings ;"):
                 logging.error("Failed to query database6")
             query.first()
             cnttxt = query.value(8) + "  >>>"
-            lcnnxt=len(cnttxt)
+            lcnnxt = len(cnttxt)
             cntname = str(query.value(8))
 
         for i in range(tkol):
             if len(logo[i]) > 0:
                 self.logo = QLabel(self)
-                self.logo.setGeometry(i * (twdt + 10) + 10, 10, twdt, hgt-mainlogoh / 4 - 10)
+                self.logo.setGeometry(i * (twdt + 10) + 10, mainlogoh + 10, twdt, hgt / 4 - 10)
                 self.logo.setAlignment(alignmentc)
-                pixmap = QPixmap(os.path.dirname(os.path.abspath(__file__))+"/./img/logo/" + logo[i])
+                pixmap = QPixmap(os.path.dirname(os.path.abspath(__file__)) + "/./img/logo/" + logo[i])
                 pixmap = pixmap.scaled(twdt, hgt / 4 - 20, Qt.KeepAspectRatio)
                 self.logo.setPixmap(pixmap)
-                self.logo.setStyleSheet("border:3px solid #99aaff;border-top-left-radius: 22px; border-top-right-radius: 22px; font-size: 48px")
+                self.logo.setStyleSheet(
+                    "border:3px solid #99aaff;border-top-left-radius: 22px; border-top-right-radius: 22px; font-size: 48px")
                 self.logo.show()
 
             self.tnm = QLabel(self)
-            #fnts = 66 - tkolt * 7
-            fnts=int(fw*1.5)
+            # fnts = 66 - tkolt * 7
+            fnts = int(fw * 1.5)
             stsh = "border:3px solid #99aaff;font-size: " + str(fnts) + "px"
             self.tnm.setStyleSheet(stsh)
             if len(logo[i]) > 0:
-                self.tnm.setGeometry(i * (twdt + 10) + 10, mainlogoh + hgt / 4, twdt, (hgt-mainlogoh) / 3 - 20)
+                self.tnm.setGeometry(i * (twdt + 10) + 10, mainlogoh + 10 + hgt / 4, twdt, hgt / 3 - 20)
             else:
-                self.tnm.setGeometry(i * (twdt + 10) + 10, mainlogoh, twdt, (hgt-mainlogoh) / 3 + hgt / 4 - 20)
-                self.tnm.setStyleSheet("border:3px solid #9999ff;border-top-left-radius: 22px; border-top-right-radius: 22px;  font-size: "+str(fnts)+"px")
-                
+                self.tnm.setGeometry(i * (twdt + 10) + 10, mainlogoh + 10, twdt, hgt / 3 + hgt / 4 - 20)
+                self.tnm.setStyleSheet(
+                    "border:3px solid #9999ff;border-top-left-radius: 22px; border-top-right-radius: 22px;  font-size: " + str(
+                        fnts) + "px")
+
             self.tnm.setWordWrap(True)
             self.tnm.setText(name[i])
             self.tnm.setAlignment(alignmentc)
@@ -258,21 +259,24 @@ class Wint(QWidget):
 
             self.result = QLabel(self)
             self.result.setObjectName("rst" + str(i))
-            self.result.setGeometry(i * (twdt + 10) + 10, hgt / 4 + hgt / 3, twdt, hgt / 5)
+            self.result.setGeometry(i * (twdt + 10) + 10, mainlogoh + hgt / 4 + hgt / 3, twdt, hgt / 5)
             self.result.setWordWrap(True)
             self.result.setText(str(tots[i]))
             self.result.setAlignment(alignmentc)
             self.result.setFont(font)
             fnts1 = 96 - tkolt * 4
-            stsh = "border:3px solid #99aaff;border-bottom-left-radius: 22px; border-bottom-right-radius: 22px;font-size: " + str(fnts1) + "px"
-            botb = "QPushButton{font-size: " + str(fnts1) + "px; border: 1px solid rgba(200,200,255,180)} QPushButton::hover{background-color: #0077ff ;} QPushButton::pressed {background-color: rgba(224, 255, 255, 195); color: rgba(0,0,255,255) }"
+            stsh = "border:3px solid #99aaff;border-bottom-left-radius: 22px; border-bottom-right-radius: 22px;font-size: " + str(
+                fnts1) + "px"
+            botb = "QPushButton{font-size: " + str(
+                fnts1) + "px; border: 1px solid rgba(200,200,255,180)} QPushButton::hover{background-color: #0077ff ;} QPushButton::pressed {background-color: rgba(224, 255, 255, 195); color: rgba(0,0,255,255) }"
             self.result.setStyleSheet(stsh)
             self.result.show()
 
             self.plusb = QPushButton(self)
             self.plusb.setObjectName("pls" + str(i))
             # QMessageBox.warning(self, "Нажматие!!!))", "Название объекта " + str(self.plusb.objectName()))
-            self.plusb.setGeometry(i * (twdt + 10) + 10, hgt / 4 + hgt / 3 + hgt / 5 + 10, twdt / 2 - 2, hgt / 10)
+            self.plusb.setGeometry(i * (twdt + 10) + 10, mainlogoh + hgt / 4 + hgt / 3 + hgt / 5 + 10, twdt / 2 - 2,
+                                   hgt / 10)
             self.plusb.setText("+")
             self.plusb.setStyleSheet(botb)
             self.plusb.clicked.connect(self.sumf)
@@ -280,40 +284,39 @@ class Wint(QWidget):
 
             self.minub = QPushButton(self)
             self.minub.setObjectName("mns" + str(i))
-            self.minub.setGeometry(i * (twdt + 10) + 12 + twdt / 2, hgt / 4 + hgt / 3 + hgt / 5 + 10, twdt / 2 - 2,
+            self.minub.setGeometry(i * (twdt + 10) + 12 + twdt / 2, mainlogoh + hgt / 4 + hgt / 3 + hgt / 5 + 10,
+                                   twdt / 2 - 2,
                                    hgt / 10)
             self.minub.setText("-")
             self.minub.setStyleSheet(botb)
             self.minub.clicked.connect(self.sumf)
             self.minub.show()
 
-
         self.catch = QPushButton(self)
-        self.catch.setGeometry(10, hgt - hgt / 15 - 10, (wdt - 25)/2,
-                                hgt / 15)
+        self.catch.setGeometry(10, mainlogoh + hgt - hgt / 15 - 10, (wdt - 25) / 2,
+                               hgt / 15)
         fs = int(1.5 * (((wdt - 25) / 2) / (lcnnxt + 2)))
         self.catch.setText("Выбор категории")
-#        self.catch.setStyleSheet("font: bold 34px; border: 1px solid rgba(200,200,255,180); border-top-right-radius: 120px 60px; border-bottom-left-radius: 180px "+str(int(hgt / 15))+"px")
-        shst="QPushButton { font: bold "+str(fs)+"px; border: 1px solid rgba(200,200,255,180); border-top-right-radius: 120px 60px; border-bottom-left-radius: 180px "+str(int(hgt / 15))+"px} QPushButton::hover{background-color: #0077ff ;} QPushButton::pressed {background-color: rgba(224, 255, 255, 195); color: rgba(0,0,255,255) }"
+        #        self.catch.setStyleSheet("font: bold 34px; border: 1px solid rgba(200,200,255,180); border-top-right-radius: 120px 60px; border-bottom-left-radius: 180px "+str(int(hgt / 15))+"px")
+        shst = "QPushButton { font: bold " + str(
+            fs) + "px; border: 1px solid rgba(200,200,255,180); border-top-right-radius: 120px 60px; border-bottom-left-radius: 180px " + str(
+            int(hgt / 15)) + "px} QPushButton::hover{background-color: #0077ff ;} QPushButton::pressed {background-color: rgba(224, 255, 255, 195); color: rgba(0,0,255,255) }"
         self.catch.setStyleSheet(shst)
         self.catch.clicked.connect(chcat)
-        if ccat>1:
+        if ccat > 1:
             self.catch.show()
         else:
             self.catch.setVisible(False)
 
-
         self.contin = QPushButton(self)
-        self.contin.setGeometry((wdt - 20)/2, hgt - hgt / 15 - 10, (wdt - 25)/2, hgt / 15)
+        self.contin.setGeometry((wdt - 25) / 2+10, mainlogoh + hgt - hgt / 15 - 10, (wdt - 25) / 2, hgt / 15)
         self.contin.setText(cnttxt)
 
-
-        self.contin.setStyleSheet("QPushButton {font: bold "+str(fs)+"px; border: 1px solid rgba(200,200,255,180);border-top-right-radius: 180px "+str(int(hgt / 15))+"px; border-bottom-left-radius: 120px 60px} QPushButton::hover{background-color: #0077ff ;} QPushButton::pressed {background-color: rgba(224, 255, 255, 195); color: rgba(0,0,255,255) }")
+        self.contin.setStyleSheet("QPushButton {font: bold " + str(
+            fs) + "px; border: 1px solid rgba(200,200,255,180);border-top-right-radius: 180px " + str(
+            int(hgt / 15)) + "px; border-bottom-left-radius: 120px 60px} QPushButton::hover{background-color: #0077ff ;} QPushButton::pressed {background-color: rgba(224, 255, 255, 195); color: rgba(0,0,255,255) }")
         self.contin.clicked.connect(cntn)
         self.contin.show()
-
-
-
 
     def sumf(self):
         # считываем цену вопроса из tmpDat settings
@@ -355,7 +358,8 @@ class Wint(QWidget):
             reply.setStyleSheet(sth)
             vbox = QVBoxLayout()
             label_dialog = QLabel()
-            label_dialog.setStyleSheet("background-color: rgba(0,0,255,240);border-top-left-radius: 80px 20px; border-bottom-right-radius: 80px 20px;")
+            label_dialog.setStyleSheet(
+                "background-color: rgba(0,0,255,240);border-top-left-radius: 80px 20px; border-bottom-right-radius: 80px 20px;")
             label_dialog.setText('Вы действительно хотите закончить игру?')
             button_yes = QPushButton(reply)
             button_yes.setText("Да")
@@ -377,8 +381,8 @@ class Wint(QWidget):
             reply.setLayout(vbox)
             reply.exec()
 
+
 if __name__ == "__main__":
-    global apt
     apt = QApplication([])
     wnt = Wint(tkolt)
     wnt.showFullScreen()
