50522b23c818d0cb7fb528af80215d94a38e1a4d
Ed <ednet@mail.ru>
2023-10-24 01:59:34 +0300

    Update

--- teams_create.py
+++ teams_create.py
@@ -13,13 +13,14 @@ shag = 2
 smx = 5
 gx = 200
 wdl=1
+kolteam=2
 
 
 class Winteamcr(QWidget):
    
     
 
-
+# Анимация фона экрана
     def paintEvent(self, event):
         global smx,blc,shag
         global gx, wd, hd
@@ -48,68 +49,55 @@ class Winteamcr(QWidget):
         # закончили с фоном
    
     def __init__(self, parent=None):
-        
         font = QFont()
         font.setFamilies([u"Arial"])
         font.setBold(True)
 
-
-
-        global gwidth, gheight,wdl
+        global gwidth, gheight,wdl, kolteam
         (gwidth, gheight) = apt.screens()[0].size().toTuple()
         super(Winteamcr, self).__init__(parent)
         wd=gwidth
         hd=gheight
         
-        #запускаем анимацию фона
+#запускаем анимацию фона
         def scrupd():
                 self.blc+=self.shag
                 if self.blc>=254 or self.blc<=60:
                     self.shag*= -1
                 self.sth="background-color: rgba(0,0,255,"+str(self.blc)+"); color: #ddFFaa;"
                 self.setStyleSheet(self.sth)
-                
+# Эффект раскрытия объектов
         def objupd():    
                    
                 if self.wdl<wd/3:
-                    self.wdl+=4
+                    self.wdl+=10
                     self.titlabel.setGeometry(wd/2-wd/6,40,self.wdl,80)
                 else:
                     self.wdl=wd/3
                     if self.badd<self.wdl/2:
-                        self.badd+=5
+                        self.badd+=10
                         self.butAddTeam.setGeometry(wd/2-wd/6,125,self.badd,40)
                     else:
                         self.badd=self.wdl/2
                         if self.spwt<self.wdl/2-5:
-                            self.spwt+=5
+                            self.spwt+=10
                             self.spinTeamCount.setGeometry(wd / 2 + 5, 125, self.spwt, 40)
                         else:
                             self.spwt=self.wdl/2-5
 
-
-
-
-                    
-
-               
-                    #self.tmrscrobj.stop()
-
-
-        # таймер фона
+# таймер фона
         self.blc = 80
         self.shag=1
         self.tmr = QTimer()  # 4
         self.tmr.timeout.connect(scrupd)
         self.tmr.start(40)
-        # конец блока анимации
 
-        #таймер элементов экрана
+#таймер элементов экрана
         self.tmrscrobj=QTimer()
         self.tmrscrobj.timeout.connect(objupd)
         self.tmrscrobj.start(8)
-        #таймер элементов экрана
 
+# Label заголовка экрана
         self.titlabel=QLabel(self)
         self.titlabel.setText("Регистрация команд")
         self.titlabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
@@ -122,7 +110,7 @@ class Winteamcr(QWidget):
         stsh = "border:0px solid #99aaff; border-radius: 10px; background-color: rgba(0,0,200,40); font-size: " + str(fnts) + "px"
         self.titlabel.setStyleSheet(stsh)
 
-
+# кнопка изменения количества команд
         def changeTeamCount():
             if self.butAddTeam.text()=="Изменить количество":
                 self.butAddTeam.setText("Подтвердить количество")
@@ -136,6 +124,8 @@ class Winteamcr(QWidget):
                     int(fnts * 0.7)) + "px}"
                 self.spinTeamCount.setStyleSheet(spstsh)
                 self.spinTeamCount.setEnabled(False)
+                kolteam=self.spinTeamCount.value()
+
         self.badd=0
         self.butAddTeam=QPushButton(self)
         self.butAddTeam.setText("Изменить количество")
@@ -143,7 +133,7 @@ class Winteamcr(QWidget):
         botb = "QPushButton{font-size: " +  str(int(fnts*0.5)) + "px; border-radius: 10px; border: 1px solid rgba(200,200,255,180); background-color: rgba(0,0,200,50)} QPushButton::hover{background-color: #0077ff ;} QPushButton::pressed {background-color: rgba(224, 255, 255, 195); color: rgba(0,0,255,255) }"
         self.butAddTeam.setStyleSheet(botb)
         self.butAddTeam.clicked.connect(changeTeamCount)
-
+# Спинер изменения количества команд
         self.spwt=0
         self.spinTeamCount=QSpinBox(self)
         self.spinTeamCount.setGeometry(wd / 2 +5, 125, self.spwt, 40)
@@ -155,6 +145,24 @@ class Winteamcr(QWidget):
         self.spinTeamCount.setMaximum(7)
         self.spinTeamCount.setEnabled(False)
         self.spinTeamCount.show()
+
+# Модуль изменения наименования команд и их логотипов
+
+        for i in range(kolteam):
+            self.lab_teamName = QLabel(self)
+            self.lab_teamName.setFont(font)
+            stshteamname = "border:0px solid #99aaff; border-radius: 20px; background-color: rgba(40,60,150,120); font-size: " + str(fnts) + "px"
+            self.lab_teamName.setStyleSheet(stshteamname)
+            self.lab_teamName.setText("Команда "+str(i+1))
+            x=40+i*(wd-80)/kolteam
+            wdnl=(wd-80)/kolteam-20
+            y=180
+            hdnl=hd-260
+            self.lab_teamName.setGeometry(x,y,wdnl,hdnl)
+            self.lab_teamName.setAlignment(Qt.AlignmentFlag.AlignCenter)
+            self.lab_teamName.show()
+        #self.lab_teamName.setGeometry()
+
 
 
 if __name__ == "__main__":
