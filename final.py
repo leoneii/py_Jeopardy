aed785db9020c5bad54d1a568d530b9830b03df0
Ed <ednet@mail.ru>
2023-12-04 01:10:04 +0300

    Update

--- final.py
+++ final.py
@@ -1,17 +1,31 @@
+import random
 import sys
 
-from PySide6.QtCore import QSize, Qt, QVariantAnimation
-from PySide6.QtGui import QPainter, QPen
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget
-
-
+from PySide6.QtCore import QSize, Qt, QVariantAnimation, QObject
+from PySide6.QtGui import QPainter, QPen, QPixmap
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QLabel
 
 
 class FinalWind(QMainWindow):
-    def __init__(self):
+
+    def __init__(self, parent= None):
         super().__init__()
+        (wdt, hgt) = app.screens()[0].size().toTuple()
         stsh="background-color: black"
         self.setStyleSheet(stsh)
+        n=5
+        k=0
+        pix=QPixmap('screenshot.png')
+
+        for i in range (n):
+            for j in range(n):
+                k+=1
+                pix1=pix.copy(i*(wdt/n),j*(hgt/n),wdt/n,hgt/n)
+                lbl=QLabel(self)
+                lbl.setObjectName("lbl"+str(k))
+                lbl.setGeometry(i*(wdt/n),j*(hgt/n),wdt/n,hgt/n)
+                lbl.setPixmap(pix1)
+
 
 
 
@@ -21,4 +35,4 @@ class FinalWind(QMainWindow):
 app = QApplication(sys.argv)
 window = FinalWind()
 window.showFullScreen()
-app.exec_()
+app.exec()
