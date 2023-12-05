391b735410968469288a7ddcc263add2fcd2721d
Ed <ednet@mail.ru>
2023-12-05 09:55:57 +0300

    Update

--- final.py
+++ final.py
@@ -1,7 +1,7 @@
 import random
 import sys
 
-from PySide6.QtCore import QSize, Qt, QVariantAnimation, QObject
+from PySide6.QtCore import QSize, Qt, QVariantAnimation, QObject, QTimer, QPoint
 from PySide6.QtGui import QPainter, QPen, QPixmap
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QLabel
 
@@ -13,26 +13,48 @@ class FinalWind(QMainWindow):
         (wdt, hgt) = app.screens()[0].size().toTuple()
         stsh="background-color: black"
         self.setStyleSheet(stsh)
-        n=170
-        k=0
+        n=2
+        spr=[]
+
+        self.k=0
         pix=QPixmap('screenshot.png')
 
         for i in range (n):
             for j in range(n):
-                k+=1
+                self.k+=1
                 pix1=pix.copy(i*(wdt/n),j*(hgt/n),wdt/n,hgt/n)
                 lbl=QLabel(self)
-                lbl.setObjectName("lbl"+str(i+1)+"w"+str(j))
+                lbl.setObjectName("lbl"+str(i)+"w"+str(j))
                 lbl.setGeometry(i*(wdt/n),j*(hgt/n),wdt/n,hgt/n)
                 lbl.setPixmap(pix1)
-        for z in range (169):
-            self.move("lbl"+str(z+1)+"w"+str(z+1), wdt/n, hgt/n )
+                spr.append(lbl)
+        vx=1
+        for i in range(1000):
+            j=0
+            for s in spr:
+                x=spr[j].pos().x()
+                y=spr[j].pos().y()
+
+                x+=vx
+                if x>=wdt or x<=0:
+                    vx*=-1
+                # y+=1
+
+                s.pos=QPoint(1,1)
+                #spr[j].move(x,y)
+                j=j+1
 
 
+    #     for i in range (n):
+    #         for j in range(n):
+    #             self.move("lbl"+str(i)+"w"+str(j), wdt/n, hgt/n )
+    #
+    # def move(self, oName, oWdt, oHgt):
+    #     obj1 = self.findChild(QLabel,oName)
+    #
+    #     obj1.setGeometry(random.randint(600,1200),random.randint(300,600),oWdt/4,oHgt/4)
 
-    def move(self, oName, oWdt, oHgt):
-        obj1 = self.findChild(QLabel,oName)
-        obj1.setGeometry(random.randint(0,800),random.randint(0,500),oWdt,oHgt)
+
 
 
 app = QApplication(sys.argv)
