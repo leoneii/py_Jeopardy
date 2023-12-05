912c2ece815621cefdef94877fac65f22125babf
Ed <ednet@mail.ru>
2023-12-06 01:14:53 +0300

    Update

--- final.py
+++ final.py
@@ -2,17 +2,18 @@ import random
 import sys
 
 from PySide6.QtCore import QSize, Qt, QVariantAnimation, QObject, QTimer, QPoint
-from PySide6.QtGui import QPainter, QPen, QPixmap
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QLabel
+from PySide6.QtGui import QPainter, QPen, QPixmap, QColor
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QLabel, QGraphicsDropShadowEffect
 
 
 class FinalWind(QMainWindow):
-    global spr
-    global s,n
-    n = 2
+    global spr,vx,vy
+    global s,n,tick
+
+    n = 15
     spr = []
-    vx = 1
-    vy = 1
+    vx =[]
+    vy=[]
     wdt = 0
     hgt = 0
     x=1
@@ -22,12 +23,12 @@ class FinalWind(QMainWindow):
         (self.wdt, self.hgt) = app.screens()[0].size().toTuple()
         stsh="background-color: black"
         self.setStyleSheet(stsh)
-
+        self.tick = 0
         self.k=0
         pix=QPixmap('screenshot.png')
         self.tmr = QTimer()
         self.tmr.timeout.connect(self.screenup)
-        self.tmr.start(10)
+        self.tmr.start()
 
 
         for i in range (n):
@@ -35,34 +36,74 @@ class FinalWind(QMainWindow):
                 self.k+=1
                 pix1=pix.copy(i*(self.wdt/n),j*(self.hgt/n),self.wdt/n,self.hgt/n)
                 lbl=QLabel(self)
-                lbl.setObjectName("lbl"+str(i)+"w"+str(j))
+                lbl.setObjectName(str(self.k))
                 lbl.setGeometry(i*(self.wdt/n),j*(self.hgt/n),self.wdt/n,self.hgt/n)
                 lbl.setPixmap(pix1)
                 spr.append(lbl)
+                tmp=random.randint(-7,7)
+                vx.append(tmp)
+                tmp = random.randint(-3, 3)
+                vy.append(tmp)
 
     def screenup(self):
-        for s in spr:
-            self.x=int(s.pos().x())
-            self.y=int(s.pos().y())
-            self.x+=self.vx
-            self.y+=self.vy
-            if self.x>=self.wdt or self.x<=0:
-                self.vx*=-1
-            if self.y>=self.hgt or self.y<=0:
-                self.vy*=-1
-            #print(x,y)
-            s.setGeometry(self.x,self.y,self.wdt/n,self.hgt/n)
+        self.tick+=1
+        self.k=0
+        self.g=0.2
+        if self.tick>25000 and self.tick<25100:
+            for s in spr:
+                self.x=int(s.pos().x())
+                self.y=int(s.pos().y())
+                dx=random.randint(-2,2)
+                self.x+=dx
+                dy = random.randint(-1, 1)
+                self.y += dy
+                s.setGeometry(self.x,self.y,self.wdt/n,self.hgt/n)
+                #self.k += 1
 
-                #     for i in range (n):
-    #         for j in range(n):
-    #             self.move("lbl"+str(i)+"w"+str(j), wdt/n, hgt/n )
-    #
-    # def move(self, oName, oWdt, oHgt):
-    #     obj1 = self.findChild(QLabel,oName)
-    #
-    #     obj1.setGeometry(random.randint(600,1200),random.randint(300,600),oWdt/4,oHgt/4)
+        self.k=0
+        if self.tick>25100:
+
+            for s in spr:
 
 
+                tmw = s.size().width()
+                tmh = s.size().height()
+                self.x=int(s.pos().x())
+                self.y=int(s.pos().y())
+                self.x+=vx[self.k]
+                vy[self.k]+=self.g
+                self.y+=vy[self.k]
+                if self.x>self.wdt or self.x<0:
+                    vx[self.k]*=-0.9
+                    if tmw>10:
+                        tmw *= 0.6
+                        tmh *= 0.6
+                if self.y>self.hgt or self.y<0:
+                    vy[self.k]*=-0.8
+                    vx[self.k]*=0.9
+                    if tmw > 10:
+                        tmw *= 0.6
+                        tmh *= 0.6
+                if tmw<=10 and self.y>self.hgt-tmh*2:
+                    if self.x<=self.wdt/2:
+                        self.x=50+random.randint(-10,10)
+                        self.dvy=-15*random.random()-5
+                        vy[self.k]=self.dvy
+                        self.dvx=random.random()*6-3
+                        vx[self.k]=self.dvx
+                    else:
+                        self.x=self.wdt-50+20*random.random()-10
+                        self.dvy = -15 * random.random() - 5
+                        vy[self.k] = self.dvy
+                        self.dvx = random.random() * 6 - 3
+                        vx[self.k] = self.dvx
+
+                s.setGeometry(self.x,self.y,tmw,tmh)
+                shadow = QGraphicsDropShadowEffect(self, blurRadius=15, offset=0, color=QColor(255, 0, 0, 200))
+                s.setGraphicsEffect(shadow)
+
+
+                self.k += 1
 
 
 app = QApplication(sys.argv)
